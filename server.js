e44dec9860a9964928cb92f122dd5dcc69b52ec3
Urkass <Kuznetpa@gmail.com>
2016-08-09 23:44:26 +0300

    Some logic added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
  */
 const express = require('express');
 const getFace = require('cool-ascii-faces');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -13,11 +14,18 @@ app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
+    response.render('index', { template: '' });
+});
+
+app.get('/answerQuestion', function(request, response) {
     console.time('render');
-    response.render('index', { face: getFace() });
+    response.render('index', { template: getFace() });
     console.timeEnd('render');
 });
 
