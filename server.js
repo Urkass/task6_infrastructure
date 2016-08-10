cbeb21875326c32d21249d6e09dec7c4a73da3fe
Urkass <Kuznetpa@gmail.com>
2016-08-10 13:46:50 +0300

    More logic added

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+let _correctAnswer;
+
 app.set('port', process.env.PORT || 5000);
 
 app.set('views', __dirname + '/views');
@@ -23,23 +25,36 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
     response.render('index', {
+        question: getEquation(),
         template: ''
     });
-});
 
+    function getEquation() {
+        let operators = ['+', '-'];
+        let equation = randomNumberRange(0, 100) + operators[randomNumberRange(0, 2)] + randomNumberRange(0, 100);
+        _correctAnswer = eval(equation);
+        console.log('Сгенерированное выражение: ', equation, ', правильный ответ: ', _correctAnswer);
+        return equation;
+    }
+
+    function randomNumberRange(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+});
 app.post('/answerQuestion', function(request, response) {
     let answer = request.body.answer;
-    const magicNumber = 2000;
+    // const magicNumber = 2000;
     checkAnswer(request.body.answer);
 
     function checkAnswer(answer) {
-        if (parseInt(answer) === magicNumber) renderFace();
+        if (parseInt(answer) === _correctAnswer) renderFace();
         else renderError();
     }
 
     function renderFace() {
         console.time('render');
         response.render('index', {
+            question: 'Все правильно!',
             template: getFace()
         });
         console.timeEnd('render');
@@ -48,6 +63,7 @@ app.post('/answerQuestion', function(request, response) {
     function renderError() {
         console.log(answer);
         response.render('index', {
+            question: '',
             template: 'Ответ неправльный!'
         });
     }
